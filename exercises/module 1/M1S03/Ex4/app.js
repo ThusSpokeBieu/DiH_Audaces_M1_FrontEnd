e9814d25958a61bf45fdc61503a48ed7de86104b
GabrielCosta <gmessiasc@gmail.com>
2022-12-15 15:51:31 -0300

    [M1S03] Ex 4 - Mesclando Objetos

---
+++ exercises/module 1/M1S03/Ex4/app.js
@@ -0,0 +1,9 @@
+    const mesclaObjetos = (objeto1, objeto2) => {
+        objeto3 = { ...objeto1, ...objeto2}
+        return objeto3;
+    }
+
+    const objUm = { a: 1, b: 2 };
+    const objDois = { c: 3, d: 4 };
+    const novoObjeto = mesclaObjetos(objUm, objDois);
+    console.log(novoObjeto);
