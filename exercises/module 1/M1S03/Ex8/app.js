c7cfd92b97ee3c3730caaca4830506a8646bf7af
GabrielCosta <gmessiasc@gmail.com>
2022-12-15 23:35:22 -0300

    [M1S03] Ex 8 - Une, Dobra e Retorna

---
+++ exercises/module 1/M1S03/Ex8/app.js
@@ -0,0 +1,11 @@
+
+const uneDobraRetorna = ([...spread], ...rest) => spread.concat(rest.map(i => i * 2));
+
+const r1 = uneDobraRetorna([1, 2, 3], 4, 4)
+console.log(r1) // [1, 2, 3, 8, 8]
+
+const r2 = uneDobraRetorna([2], 10, 4, 8) 
+console.log(r2) // [2, 20, 8, 16]
+
+const r3 = uneDobraRetorna([6, 8]) 
+console.log(r3) // [6, 8]
