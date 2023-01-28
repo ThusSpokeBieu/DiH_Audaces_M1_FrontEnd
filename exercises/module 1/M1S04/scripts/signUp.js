0ab98f8772463093e629a63a38001e0f640b2cc7
Gabriel Messias Costa <gmessiasc@gmail.com>
2023-01-28 13:49:11 -0300

    [M1S04]: Exercicio 4, lógica para registrar

---
+++ exercises/module 1/M1S04/scripts/signUp.js
@@ -0,0 +1,127 @@
+if(localStorage.length === 0){
+  const users = [
+    {
+    name: "Devin House",
+    cpf: "00000000000",
+    celular: "99999999999",
+    senha: "adm1n_Dev1n",
+    conta: new Date().getTime(),
+    saldo: 99999,
+    }
+  ]
+
+  localStorage.setItem("users", JSON.stringify(users));
+}
+
+console.log(localStorage.getItem("users"))
+
+const form = document.querySelector("form");
+const nameInput = document.getElementById("name");
+const cpfInput = document.getElementById("cpf");
+const celularInput = document.getElementById("celular");
+const saltRounds = 10;
+
+nameInput?.addEventListener("input", () => {
+  let name = nameInput.value;
+  name = name.replace(/\d/g, "");
+
+  nameInput.value = name;
+
+});
+
+cpfInput?.addEventListener("input", () => {
+  let cpf = cpfInput.value;
+  cpf = cpf.replace(/[^\d]/g, "");
+
+  if (cpf.length > 11) {
+    cpf = cpf.slice(0, 11);
+  }
+
+  cpfInput.value = cpf;
+});
+
+celularInput?.addEventListener("input", () => {
+  let celular = celularInput.value;
+  celular = celular.replace(/[^\d]/g, "");
+
+  if (celular.length > 11) {
+    celular = celular.slice(0, 11);
+  }
+
+  celularInput.value = celular;
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const inputs = document.querySelectorAll("input");
+
+  for (let input of inputs) {
+    if (input.value === "") {
+      alert("Todos os campos devem ser preenchidos");
+      return;
+    }
+  }
+
+  const password = document.querySelector("#pwd").value;
+  const confirmPassword = document.querySelector("#pwd-confirm").value;
+  if (password !== confirmPassword) {
+    alert("As senhas não coincidem");
+    return;
+  }
+
+  const cpf = document.querySelector("#cpf").value;
+  if (!validateCpf(cpf)) {
+    alert("CPF inválido");
+    return;
+  }
+
+  const celular = document.querySelector("#celular").value;
+  if (!validateCelular(celular)) {
+    alert("Celular inválido");
+    return;
+  }
+
+  let re = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/
+  
+  if(!re.test(password)){
+    alert(`Senha fraca, por favor, insira uma senha com os requisitos: \n - Pelo menos 8 caracteres. \n - Pelo menos um número. \n - Pelo menos um caracter maiusculo. \n - Pelo menos um caracter minusculo. \n - Pelo menos um símbolo. `)
+    return 
+  }
+
+  
+  let newUser = {
+    name: nameInput.value,
+    cpf: cpfInput.value,
+    celular: celular,
+    senha: password,
+    conta: new Date().getTime(),
+    saldo: 0,
+  }
+
+  let users = JSON.parse(localStorage.getItem("users"));
+  users.push(newUser);
+  localStorage.setItem("users", JSON.stringify(users));
+
+  
+  form.submit();
+  alert(`Parabéns ${newUser.name}, sua conta foi criada. \n
+  \n Seu número de conta é ${newUser.conta}, use seu cpf e senha para entrar.`)
+
+});
+
+function validateCpf(cpf) {
+   if (new Set(cpf.split("")).size === 1) {
+     return false;
+   }
+   if (cpf.length !== 11) return false;
+   return true;
+}
+
+function validateCelular(cellphone) {
+    cellphone = cellphone.replace(/[^\d]+/g,'');
+
+    if (cellphone.length !== 11) return false;
+
+    return true;
+}
