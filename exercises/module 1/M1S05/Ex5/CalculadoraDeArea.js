17bef2d22ea2573f1a3ae8163042269e7f081a17
Gabriel Messias Costa <gmessiasc@gmail.com>
2023-02-04 09:33:26 -0300

    [M1S05]: Ex5 - Classe Área

---
+++ exercises/module 1/M1S05/Ex5/CalculadoraDeArea.js
@@ -0,0 +1,25 @@
+export default class CalculadoraDeArea {
+  tipo;
+  base;
+  altura;
+  
+  constructor(tipo, base, altura) {
+    this.tipo = tipo;
+    this.base = base;
+    this.altura = altura;
+  }
+
+  calcular() {
+    switch (this.tipo) {
+      case "triangulo":
+        return (this.base * this.altura) / 2;
+      case "quadrado":
+      case "retangulo":
+        return this.base * this.altura;
+      default:
+        return 0;
+    }
+  }
+}
+
+
