3ad345b68cd2c489e8c892cedc824bb54cbcff2a
Gabriel Messias Costa <gmessiasc@gmail.com>
2023-02-12 13:30:46 -0300

    [M1S06]: Exercise 3: promise all

---
+++ exercises/module 1/M1S06/Ex3/promiseAll.js
@@ -0,0 +1,36 @@
+function aprovarDadosCadastrais( cliente ) {
+  return new Promise( (resolve, reject) => {
+      if (cliente.cpf === 11111111111)
+          resolve(true);
+      else 
+          reject(false);
+  });
+}
+
+function aprovarValorSolicitado( cliente, valor ) {
+  return new Promise( (resolve, reject) => {
+      if (cliente.margem >= valor)
+          resolve(true);
+      else 
+          reject(false);
+  });
+}
+
+const cliente = { cpf: 11111111111, nome: 'Calleri', margem: 1000.0 };
+const aprovacaoCadastro = aprovarDadosCadastrais(cliente);
+const aprovacaoValor = aprovarValorSolicitado(cliente, 300.0);
+
+Promise.all([aprovacaoCadastro, aprovacaoValor])
+  .then(([aprovacaoCadastro, aprovacaoValor]) => {
+    if (aprovacaoCadastro && aprovacaoValor) {
+      console.log("APROVADO");
+    } else {
+      console.log("REPROVADO");
+    }
+  })
+  .catch(() => {
+    console.log("REPROVADO");
+  })
+  .finally(() => {
+    console.log("Fim do Processamento");
+  });
